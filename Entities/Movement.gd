620092ef2ceb2d85475e791bff7710ba12303404
matgib38 <82478253+matgib38@users.noreply.github.com>
2021-07-06 10:55:04 +1200

    added spike floor long

--- Entities/Movement.gd
+++ Entities/Movement.gd
@@ -23,7 +23,7 @@ func get_input(): #Codes for movement
 	var input = Vector3(
 		0,
 		0,
-		-int(Input.is_action_pressed("ui_left")) + int(Input.is_action_pressed("ui_right"))
+		-int(Input.is_action_pressed("left")) + int(Input.is_action_pressed("right"))
 	)
 	input = input.normalized()
 	return input
@@ -62,20 +62,20 @@ func _physics_process(delta):
 	if is_on_wall():
 		can_double_jump = true
 		
-	if Input.is_action_just_pressed("ui_up") and not is_on_floor() and double_jump == true and can_double_jump:
+	if Input.is_action_just_pressed("jump") and not is_on_floor() and double_jump == true and can_double_jump:
 		move_vector.y = jump
 		can_double_jump = false
 		
-	if Input.is_action_just_pressed("ui_up") and is_on_floor(): #Codes for basic jump
+	if Input.is_action_just_pressed("jump") and is_on_floor(): #Codes for basic jump
 		move_vector.y = jump
 	
-	if Input.is_action_just_pressed("ui_up") and is_on_wall() and wall_jump == true and can_wall_jump: #Codes for wall jump
+	if Input.is_action_just_pressed("jump") and is_on_wall() and wall_jump == true and can_wall_jump: #Codes for wall jump
 		move_vector.y = jump
 		can_wall_jump = false
 		can_double_jump = true
 		$WallJumpTimer.start()
 	
-	if Input.is_action_just_pressed("ui_down") and dash and can_dash == true:
+	if Input.is_action_just_pressed("do_dash") and dash and can_dash == true:
 		speed = 50
 		max_speed = 50
 		dashing = true
