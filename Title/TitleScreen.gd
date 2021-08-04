dea6abfc1e647a63831d876a8547d776140de4ee
matgib38 <82478253+matgib38@users.noreply.github.com>
2021-08-05 10:42:25 +1200

    added title screen buttons, which need to link to new areas now

---
+++ Title/TitleScreen.gd
@@ -0,0 +1,11 @@
+extends Control
+
+
+func _on_Startbutton_pressed():
+	get_tree().change_scene("res://Scenes/Test Level.tscn")
+
+func _on_Controlbutton_pressed():
+	get_tree().change_scene("res://Scenes/ControlMenu.tscn")
+
+func _on_Exitbutton_pressed():
+	get_tree().quit()
