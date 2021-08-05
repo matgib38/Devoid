69b20e406a0e441f27795fae11cf2a44ffcbb526
matgib38 <82478253+matgib38@users.noreply.github.com>
2021-08-05 20:41:55 +1200

    did stuff on open night, messed with platforms and fixed the problem with both the menu screen and platforms and spikes.

--- Title/ControlMenu.gd
+++ Title/ControlMenu.gd
@@ -1,5 +1,4 @@
 extends Control
 
-
 func _on_ReturnMenu_pressed():
 	get_tree().change_scene("res://Scenes/TitleScreen.tscn")
--- Title/TitleScreen.gd
+++ Title/TitleScreen.gd
@@ -1,6 +1,5 @@
 extends Control
 
-
 func _on_Startbutton_pressed():
 	get_tree().change_scene("res://Scenes/Test Level.tscn")
 
