2bf0b76c3091574e9b7315bb2f86fc82353f4f70
matgib38 <82478253+matgib38@users.noreply.github.com>
2021-10-26 09:40:58 +1300

    walljump was broken, now fixed

--- Entities/WallJump.gd
+++ Entities/WallJump.gd
@@ -1,8 +1,6 @@
 extends Spatial
 
-onready var wall_check = get_parent().get_node("Player")
-
 func _on_Area_body_entered(body):
 	if body.name == "Player":
-		wall_check.wall_jump = true
+		Health.wall_jump = true
 		queue_free()
