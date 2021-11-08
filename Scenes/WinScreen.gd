5e3ba02c69b982e5d4dd25d3cbd58dfecd1b2b2b
matgib38 <82478253+matgib38@users.noreply.github.com>
2021-11-08 14:01:06 +1300

    fixed enviroment\

--- Scenes/WinScreen.gd
+++ Scenes/WinScreen.gd
@@ -5,3 +5,6 @@ func _on_ExitButton_pressed():
 
 func _on_RestartButton_pressed():
 	get_tree().change_scene("res://Scenes/TitleScreen.tscn")
+	Health.wall_jump = false
+	Health.dash = false
+	Health.double_jump = false
